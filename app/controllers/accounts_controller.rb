8ff9818b1bd545bdcb278ed13772e048b5975710
Newton <altontonnalumasa@gmail.com>
2023-03-03 19:24:56 +0300

    Add app functionality: add transaction, display transaction add category, display category add unit test

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,10 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group, only: %i[index create new]
-
   def index
+    # @group = Group.find(params[:group_id])
+    # @accounts = Account.where(group_id: @group, user_id: current_user.id)
+    # @accounts = Account.includes(:user).where(user_id: current_user.id).order(created_at: :desc)
     @accounts = @group.accounts.order(created_at: :desc)
     @accounts_sum = @accounts.sum(:amount)
   end
@@ -17,7 +19,7 @@ class AccountsController < ApplicationController
     @account.user_id = current_user.id
     if @account.save
       GroupTransaction.create!(group_id: @group.id, account_id: @account.id)
-      redirect_to new_group_account_path(@account), notice: "Transaction created successfully"
+      redirect_to new_group_account_path(@account), notice: 'Transaction created successfully'
     else
       render :new, status: :unprocessable_entity
     end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :confirmable, :validatable
+
+  has_many :groups, foreign_key: :user_id
+  has_many :accounts, foreign_key: :user_id
+
+  validates :name, presence: true
+end
